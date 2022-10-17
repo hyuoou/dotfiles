22cfd169f3503c41dfb1069f925f9debfe5e734c
hyuoou <63180765+hyuoou@users.noreply.github.com>
2022-10-17 11:03:26 +0900

    Update

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 xrandr --output HDMI-0 --mode 3440x1440 --rate 99.99 &
-nitrogen --restore
-sleep 1
-picom -b
+nitrogen --restore &
+sleep 1 &
+picom -b &
 fcitx5 &
 imwheel -b 45 &
 nm-applet &
