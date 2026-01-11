d6f4442fd644571be8c170e248ca13277e7c221d
hyuoou <63180765+hyuoou@users.noreply.github.com>
2026-01-11 20:22:31 +0900

    Update

--- dot_config/sway/executable_autostart.sh
+++ dot_config/sway/executable_autostart.sh
@@ -5,3 +5,14 @@ nm-applet &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/bin/gnome-keyring-daemon --start --components=secrets &
 blueman-applet &
+swaync &
+wl-paste --type text --watch cliphist store -max-items 100 &
+wl-paste --type image --watch cliphist store &
+hash dbus-update-activation-environment 2>/dev/null &&
+  dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway &
+./import-gsettings.sh \
+  gtk-theme:Matcha-dark-azul \
+  icon-theme:Papirus-Dark \
+  cursor-theme:Breeze5 &
+spotify &
+thunderbird &
